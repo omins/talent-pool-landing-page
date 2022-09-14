3af5dcb353f9cd2a928d85739fea277456fa7a2e
Kevin Minseok Oh <oms3749@gmail.com>
2022-09-15 00:26:34 +0900

    feat: Google Analytics 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import Head from 'next/head';
 import '../../styles/globals.css';
 
@@ -10,6 +11,26 @@ function MyApp({ Component, pageProps }) {
           content='width=device-width,initial-scale=1'
         ></meta>
       </Head>
+      <Script
+        strategy='afterInteractive'
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+      });
+    `,
+        }}
+      />
+
       <Component {...pageProps} />
     </>
   );
